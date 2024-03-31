2da3349761a6698a6e7622b060df42763fa255cd
Andrew Cleaves <andrewbcleaves@gmail.com>
2024-03-31 13:06:11 -0400

    maybe this

--- lessons/02-Conditionals/index.js
+++ lessons/02-Conditionals/index.js
@@ -21,10 +21,10 @@ const givenTwo = "cheese" === "cheese"; // true
 const givenThree = 15 / 3 === 5; // true
 const givenFour = 12 > 13; // false
 
-const andTrue = (givenTwo && givenThree)
-const andFalse = (givenOne && givenFour)
-const orTrue = (givenTwo || givenFour)
-const orFalse = (givenOne = givenThree)
+const andTrue === true
+const andFalse === false
+const orTrue == true
+const orFalse == false
 
 /**
  * #2: fiveCharacters
